6fe6c0b193283c3221440ce8049bf89e1d5491ae
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-03-22 08:01:36 -0500

    initial commit

---
+++ Roomates/Repositories/ChoreRepository.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    public class ChoreRepository : BaseRepository
+    {
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Chore";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Chore> chores = new List<Chore>();
+
+                        while (reader.Read())
+                        {
+                            int idColumnPosition = reader.GetOrdinal("Id");
+
+                            int idValue = reader.GetInt32(idColumnPosition);
+
+                            int nameColumnPosition = reader.GetOrdinal("Name");
+                            string nameValue = reader.GetString(nameColumnPosition);
+
+                            Chore chore = new Chore
+                            {
+                                Id = idValue,
+                                Name = nameValue,
+                            };
+
+                            chores.Add(chore);
+                        }
+                        return chores;
+                    }
+
+                }
+            }
+        }
+        public Chore GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name FROM Chore WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Chore chore = null;
+
+                        if (reader.Read())
+                        {
+                            chore = new Chore
+                            {
+                                Id = id,
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                            };
+                        }
+                        return chore;
+                    }
+
+                }
+            }
+        }
+        public void Update(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Chore
+                                    SET Name = @name
+                                    WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    cmd.Parameters.AddWithValue("@id", chore.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Chore (Name) 
+                                         OUTPUT INSERTED.Id 
+                                         VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+                }
+            }
+        }
+        public List<Chore> GetUnassignedChores() 
+        {
+            using (SqlConnection conn = Connection)
+            { 
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Chore.Id AS ChoreId, Chore.Name AS ChoreName
+                                        FROM Chore LEFT JOIN RoommateChore ON Chore.Id = RoommateChore.ChoreId
+                                        WHERE RoommateId is null";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Chore> unassignedChores = new List<Chore>();
+
+                        while (reader.Read())
+                        {
+                            Chore chore = new Chore 
+                            { 
+                                Id = reader.GetInt32(reader.GetOrdinal("ChoreId")),
+                                Name = reader.GetString(reader.GetOrdinal("ChoreName")),
+                            };
+                            unassignedChores.Add(chore);
+                        }
+                        return unassignedChores;
+                    }
+                }
+            }
+        }
+
+        public void AssignChore(int roommateId, int choreId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO RoommateChore (RoommateId, ChoreId) 
+                                        VALUES (@rmid, @cid)";
+                    cmd.Parameters.AddWithValue("@rmid", roommateId);
+                    cmd.Parameters.AddWithValue("@cid", choreId);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Roomates/Repositories/RoommateRepository.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    public class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Chore.Id AS ChoreId, Chore.Name AS ChoreName
+                                        FROM Chore LEFT JOIN RoommateChore ON Chore.Id = RoommateChore.ChoreId
+                                        WHERE RoommateId is null";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Roommate roommate = null;
+
+                        if (reader.Read())
+                        {
+                            roommate = new Roommate
+                            {
+                                Id = id,
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                                Room = new Room()
+                                {
+                                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                                    MaxOccupancy = reader.GetInt32(reader.GetOrdinal("MaxOccupancy"))
+                                }
+                            };
+                        }
+                        return roommate;
+                    }
+
+                }
+            }
+        }
+
+        public List<Roommate> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, FirstName, LastName FROM Roommate";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Roommate> roommates = new List<Roommate>();
+
+                        while (reader.Read())
+                        {
+                            int idColumnPosition = reader.GetOrdinal("Id");
+                            int idValue = reader.GetInt32(idColumnPosition);
+
+                            int firstNameColumnPosition = reader.GetOrdinal("FirstName");
+                            string firstNameValue = reader.GetString(firstNameColumnPosition);
+
+                            int lastNameColumnPosition = reader.GetOrdinal("LastName");
+                            string lastNameValue = reader.GetString(lastNameColumnPosition);
+
+                            Roommate roommate = new Roommate
+                            {
+                                Id = idValue,
+                                FirstName = firstNameValue,
+                                LastName = lastNameValue
+                            };
+
+                            roommates.Add(roommate);
+                        }
+                        return roommates;
+                    }
+
+                }
+            }
+        }
+    }
+}
